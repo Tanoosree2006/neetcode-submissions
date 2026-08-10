abe8e7866b9af1771cbf39d721d03e4c6cd15c24
Tanoo sree Krosuru <147473260+Tanoosree2006@users.noreply.github.com>
2026-08-10 21:21:34 +0530

    Add: take-gifts-from-the-richest-pile - submission-0

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq(gifts.begin(),gifts.end());
+        for(int i=0;i<k;i++){
+            int n= pq.top();pq.pop();
+            pq.push((int)floor(sqrt(n)));
+        }
+        long long sum=0;
+        while(!pq.empty()){
+            sum+=pq.top();pq.pop();
+        }
+        return sum;
+    }
+};
